d918569d926f4db275a8a69dfec2937459f16e08
MaggieHMD <maggie_hmd@163.com>
2021-09-27 10:20:42 +0800

    stop project

--- preactresnet34-cifar10-stdtrain-20210924.sh
+++ preactresnet34-cifar10-stdtrain-20210924.sh
@@ -1,14 +1,20 @@
 source ~/.bashrc
 source /home/xieyi/anaconda3/bin/activate mmat
 
-# preactresnet34 cifar10 stdftrain 20210924000  lr = 0.01
-CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 20 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
+# # preactresnet34 cifar10 stdftrain 20210924000  lr = 0.01
+# CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 20 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
 
-# preactresnet34 cifar10 stdftrain 20210924000  lr = 0.001
-CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 20 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
+# # preactresnet34 cifar10 stdftrain 20210924000  lr = 0.001
+# CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 20 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
 
-# preactresnet34 cifar10 stdftrain 20210924000  lr = 0.01
+# # preactresnet34 cifar10 stdftrain 20210924000  lr = 0.01
+# CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 40 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
+
+# # preactresnet34 cifar10 stdftrain 20210924000  lr = 0.001
+# CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 40 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
+
+# preactresnet34 cifar10 stdftrain 20210926000  lr = 0.01
 CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 40 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
 
-# preactresnet34 cifar10 stdftrain 20210924000  lr = 0.001
+# preactresnet34 cifar10 stdftrain 20210926001  lr = 0.001
 CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20210924.py run --mode train --train_mode cla-train --exp_name preactresnet34-cifar10 --cla_model preactresnet34 --dataset cifar10 --batch_size 256 --epochs 40 --lr 0.01 >> /home/maggie/mmat/log/preactresnet34-cifar10-stdtrain/preactresnet34-cifar10-stdtrain-20210924.log 2>&1
