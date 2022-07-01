532e8020ee4fcd664b4ff3209e61cb66f8f4787d
MaggieHMD <maggie_hmd@163.com>
2022-07-01 19:57:15 +0800

    night

--- ImagenetMixed10-stylegan2ada-train-20220710.sh
+++ ImagenetMixed10-stylegan2ada-train-20220710.sh
@@ -1,7 +1,7 @@
 source ~/.bashrc
 source /home/xieyi/anaconda3/bin/activate mmat
 
-# train stylegan2ada for cifar100
+# train stylegan2ada for imagenetmixed10
 #--------------------
-python tasklauncher-20220701.py run --save_path /maggie/result --exp_name stylegan2ada-imagenetmixed10 --gen_model stylegan2ada --dataset imagenetmixed10 --data ~/autodl-tmp/maggie/data/imagenetmixed10/imagenetmixed104stylegan2ada/datasets/imagenetmixed10.zip --epochs 1 --batch_size 64 --mode train --train_mode gen-train 
+python tasklauncher-20220701.py run --save_path /root/autodl-tmp/maggie/result --save_path /maggie/result --exp_name stylegan2ada-imagenetmixed10 --gen_model stylegan2ada --dataset imagenetmixed10 --data /root/autodl-tmp/maggie/data/imagenetmixed10/imagenetmixed104stylegan2ada/datasets/imagenetmixed10.zip --epochs 1 --batch_size 64 --mode train --train_mode gen-train 
 
