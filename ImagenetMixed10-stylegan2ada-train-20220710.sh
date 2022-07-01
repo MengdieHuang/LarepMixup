45615c9b9f6c2788d441e44812277f0082329b3c
MaggieHMD <maggie_hmd@163.com>
2022-07-01 20:03:02 +0800

    train mixed10

--- ImagenetMixed10-stylegan2ada-train-20220710.sh
+++ ImagenetMixed10-stylegan2ada-train-20220710.sh
@@ -3,5 +3,5 @@ source /home/xieyi/anaconda3/bin/activate mmat
 
 # train stylegan2ada for imagenetmixed10
 #--------------------
-python tasklauncher-20220701.py run --save_path /root/autodl-tmp/maggie/result --save_path /maggie/result --exp_name stylegan2ada-imagenetmixed10 --gen_model stylegan2ada --dataset imagenetmixed10 --data /root/autodl-tmp/maggie/data/imagenetmixed10/imagenetmixed104stylegan2ada/datasets/imagenetmixed10.zip --epochs 1 --batch_size 64 --mode train --train_mode gen-train 
+python tasklauncher-20220701.py run --save_path /root/autodl-tmp/maggie/result --save_path /maggie/result --exp_name stylegan2ada-imagenetmixed10 --gen_model stylegan2ada --dataset imagenetmixed10 --data /root/autodl-tmp/maggie/data/imagenetmixed10/imagenetmixed104stylegan2ada/datasets/imagenetmixed10.zip --epochs 1 --batch_size 64 --mode train --train_mode gen-train --gpus 1
 
