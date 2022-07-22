c1f82d2cfe69c05571dc047c52a7aa47d6e0b723
MaggieHMD <maggie_hmd@163.com>
2022-07-22 15:22:58 +0800

    localserver-update cw

--- svhn-generate-autoattack-3models-20220721.sh
+++ svhn-generate-autoattack-3models-20220721.sh
@@ -11,4 +11,10 @@ CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20220720.py run --mode attack --at
 #----------------------preactresnet50---------------------------------
 CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20220720.py run --mode attack --attack_mode autoattack  --whitebox --exp_name preactresnet50-svhn --cla_model preactresnet50 --cla_network_pkl /home/data/maggie/result-newhome/train/cla-train/preactresnet50-svhn/20220627/00001-testacc-95.76/train-svhn-dataset/standard-trained-classifier-preactresnet50-on-clean-svhn-epoch-0013.pkl --dataset svhn --attack_eps 0.1 >> /home/maggie/mmat/log/SVHN/attack-example-generate/autoattack/svhn-preactresnet50-autoattack-generate-20220721.log 2>&1
 
+# generate cifar10 preactresnet50 cw adversarial examples
+#----------------------preactresnet50---------------------------------
+CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20220720.py run --save_path /home/data/maggie/result-newhome --mode attack --attack_mode cw --whitebox --exp_name preactresnet50-cifar10 --cla_model preactresnet50 --cla_network_pkl /home/data/maggie/result-newhome/train/cla-train/preactresnet50-cifar10/20210925/00003-testacc-84.74/train-cifar10-dataset/standard-trained-classifier-preactresnet50-on-clean-cifar10-epoch-0021.pkl --dataset cifar10 >> /home/maggie/mmat/log/CIFAR10/attack-example-generate/cw/cifar10-preactresnet50-cw-generate-20220721.log 2>&1
 
+# generate svhn preactresnet50 cw adversarial examples
+#----------------------preactresnet50---------------------------------
+CUDA_VISIBLE_DEVICES=0 python -u tasklauncher-20220720.py run --save_path /home/data/maggie/result-newhome --mode attack --attack_mode cw --whitebox --exp_name preactresnet50-svhn --cla_model preactresnet50 --cla_network_pkl /home/data/maggie/result-newhome/train/cla-train/preactresnet50-svhn/20220627/00001-testacc-95.76/train-svhn-dataset/standard-trained-classifier-preactresnet50-on-clean-svhn-epoch-0013.pkl --dataset svhn >> /home/maggie/mmat/log/SVHN/attack-example-generate/cw/svhn-preactresnet50-cw-generate-20220721.log 2>&1
