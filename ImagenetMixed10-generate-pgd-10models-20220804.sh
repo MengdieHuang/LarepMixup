2eec4ec16e9ee4d021403e46a62b3d6bf8f3bab0
MaggieHMD <maggie_hmd@163.com>
2022-08-05 16:06:26 +0800

    codeserver-pgd generate

--- ImagenetMixed10-generate-pgd-10models-20220804.sh
+++
@@ -1,7 +0,0 @@
-source ~/.bashrc
-# source /home/xieyi/anaconda3/bin/activate mmat
-source /root/miniconda3/bin/activate mmat
-
-# imagenetmixed10 pgd generate 20220804
-#----------------------preactresnet18---------------------------------
-python tasklauncher-20220804.py run --save_path /home/data/maggie/result-newhome --mode attack --attack_mode pgd --whitebox --exp_name preactresnet18-imagenetmixed10 --cla_model preactresnet18 --cla_network_pkl /root/autodl-tmp/maggie/result/train/cla-train/preactresnet18-imagenetmixed10/standard-trained-classifier-preactresnet18-on-clean-imagenetmixed10-epoch-0023-acc-90.47.pkl --dataset imagenetmixed10 --attack_eps 0.02 --batch_size 16 >> /root/autodl-nas/maggie/mmat/log/ImagenetMixed10/attack-example-generate/pgd/imagenetmixed10-preactresnet18-pgd-generate-20220804.log 2>&1
